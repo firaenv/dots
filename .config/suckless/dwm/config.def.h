6cdd41e1a03bb07e9d04e226e77217ae7297888f
Tyler Kelley <tinyhomegamer@criptext.com>
2021-08-15 00:13:28 -0500

    add st config and add/update profiles

--- .config/suckless/dwm/config.def.h
+++ .config/suckless/dwm/config.def.h
@@ -58,12 +58,13 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-p", "Run: ", "-fn", dmenufont, "-nb", col_1, "-nf", col_3, "-sb", col_4, "-sf", col_3, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char *gimpcmd[]  = { "gimp", NULL };
 static const char *filecmd[]  = { "pcmanfm", NULL };
 static const char *webcmd[]   = { "qutebrowser", NULL };
 static const char *vlccmd[]   = { "vlc", NULL };
 static const char *obscmd[]   = { "obs", NULL };
+static const char *scrpcmd[]  = { "config-sel", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -74,6 +75,7 @@ static Key keys[] = {
 	{ MODKEY,			XK_w,      spawn,          {.v = webcmd } },
 	{ MODKEY,			XK_v,      spawn,          {.v = vlccmd } },
 	{ MODKEY,			XK_o,      spawn,          {.v = obscmd } },
+	{ MODKEY,			XK_s,      spawn,          {.v = scrpcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
